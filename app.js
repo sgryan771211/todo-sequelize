b0e4fc790bc210dd7777ee3a7decf460bdbf1b3f
dongdong <sgryan771211@gmail.com>
2019-05-18 21:47:28 +0800

    add home and user routers

--- app.js
+++ app.js
@@ -19,35 +19,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 // 設定路由
+// 使用路由器
+app.use('/users', require('./routes/user'))
 // 首頁
 app.get('/', (req, res) => {
   res.send('hello world')
 })
-// 認證系統的路由
-// 登入頁面
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
-// 登入檢查
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
-// 註冊頁面
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-// 註冊檢查
-app.post('/users/register', (req, res) => {
-  User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  }).then(user => res.redirect('/'))
-})
-// 登出
-app.get('/users/logout', (req, res) => {
-  res.send('logout')
-})
+
 // 設定 express port 3000
 app.listen(port, () => {
   db.sequelize.sync()
