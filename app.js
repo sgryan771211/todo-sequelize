527f80852366ec02e441773d64451b3d04431c92
dongdong <sgryan771211@gmail.com>
2019-05-19 17:07:55 +0800

    Add facebook login

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
-
+// 判別開發環境
+if (process.env.NODE_ENV !== 'production') {      // 如果不是 production 模式
+  require('dotenv').config()                                       // 使用 dotenv 讀取 .env 檔案
+}
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
@@ -40,6 +43,7 @@ app.use((req, res, next) => {
 app.use('/', require('./routes/home'))
 app.use('/users', require('./routes/user'))
 app.use('/todos', require('./routes/todo'))
+app.use('/auth', require('./routes/auths'))
 
 // 設定 express port 3000
 app.listen(port, () => {
